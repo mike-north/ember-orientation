ccafa98a13f44e3024f6c5b3de2620e61d364430
Mike North <northm@yahoo-inc.com>
2015-06-01 13:29:16 -0700

    didTilt fires, tiltAlpha, tiltBeta, tiltGamma work via mixin

--- app/initializers/device-orientation.js
+++ app/initializers/device-orientation.js
@@ -5,9 +5,9 @@ export function initialize(_container, application) {
   const { orientationServiceDefaults } = config;
   const { injectionFactories } = orientationServiceDefaults;
 
-  application.register('config:device-orientation-service', orientationServiceDefaults, { instantiate: false });
+  application.register('config:device-orientation', orientationServiceDefaults, { instantiate: false });
   application.register('service:device-orientation', DeviceOrientationService);
-  application.inject('service:device-orientation', 'orientationServiceDefaults', 'config:device-orientation-service');
+  application.inject('service:device-orientation', 'orientationServiceDefaults', 'config:device-orientation');
 
   injectionFactories.forEach(factory => {
     application.inject(factory, 'orientation', 'service:device-orientation');
