6963c731fda2fc5f85fe2cc6961e60d41a685bfe
Mike North <northm@yahoo-inc.com>
2015-06-01 12:04:51 -0700

    fixes

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -41,7 +41,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.baseURL = '/ember-resize';
+    ENV.baseURL = '/ember-orientation';
   }
 
   return ENV;
