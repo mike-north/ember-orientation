0b50ad4370181f61292414827259d72f0b1f7c72
Michael North <michael.l.north@gmail.com>
2018-01-02 22:24:34 -0800

    Ember 2.17

--- tests/dummy/app/components/x-example.js
+++ tests/dummy/app/components/x-example.js
@@ -1,18 +1,19 @@
-import Ember from 'ember';
+import { computed } from '@ember/object';
+import Component from '@ember/component';
+import { htmlSafe } from '@ember/string';
 import DeviceOrientationAware from 'ember-orientation/mixins/device-orientation-aware';
 
-const { computed, Component, String: { htmlSafe } } = Ember;
-
 export default Component.extend(DeviceOrientationAware, {
-
   classNames: ['x-example'],
 
   transformStyle: computed('tiltAlpha', 'tiltBeta', 'tiltGamma', function() {
-    return htmlSafe(`transform: rotateZ(${(this.get('tiltAlpha'))}deg) `
-      + `rotateX(${-this.get('tiltBeta')}deg) `
-      + `rotateY(${-this.get('tiltGamma')}deg);`
-      + `-webkit-transform: rotateZ(${(this.get('tiltAlpha'))}deg) `
-      + `rotateX(${-this.get('tiltBeta')}deg) `
-      + `rotateY(${-this.get('tiltGamma')}deg)`);
+    return htmlSafe(
+      `transform: rotateZ(${this.get('tiltAlpha')}deg) ` +
+        `rotateX(${-this.get('tiltBeta')}deg) ` +
+        `rotateY(${-this.get('tiltGamma')}deg);` +
+        `-webkit-transform: rotateZ(${this.get('tiltAlpha')}deg) ` +
+        `rotateX(${-this.get('tiltBeta')}deg) ` +
+        `rotateY(${-this.get('tiltGamma')}deg)`
+    );
   })
 });
--- tests/unit/initializers/device-orientation-test.js
+++ tests/unit/initializers/device-orientation-test.js
@@ -1,8 +1,7 @@
-import Ember from 'ember';
+import Application from '@ember/application';
+import { run } from '@ember/runloop';
 import { initialize } from '../../../initializers/device-orientation';
 import { module, test } from 'qunit';
-
-const { Application, run } = Ember;
 
 let container, application;
 
@@ -18,20 +17,35 @@ module('Unit | Initializer | device-orientation', {
 });
 
 test('service is registered to the container', function(assert) {
-  assert.ok(!application.__container__.lookup('service:device-orientation'), 'not registered as service:device-orientation in the container before initializer');
+  assert.ok(
+    !application.__container__.lookup('service:device-orientation'),
+    'not registered as service:device-orientation in the container before initializer'
+  );
   initialize(container, application);
-  assert.ok(application.__container__.lookup('service:device-orientation'), 'registered as service:device-orientation in the container after initializer');
+  assert.ok(
+    application.__container__.lookup('service:device-orientation'),
+    'registered as service:device-orientation in the container after initializer'
+  );
 });
 
 test('service configuration is registered to the container', function(assert) {
-  assert.ok(!application.__container__.lookup('config:device-orientation'), 'not registered as config:device-orientation in the container before initializer');
+  assert.ok(
+    !application.__container__.lookup('config:device-orientation'),
+    'not registered as config:device-orientation in the container before initializer'
+  );
   initialize(container, application);
-  assert.ok(application.__container__.lookup('config:device-orientation'), 'registered as config:device-orientation in the container after initializer');
+  assert.ok(
+    application.__container__.lookup('config:device-orientation'),
+    'registered as config:device-orientation in the container after initializer'
+  );
 });
 
 test('service configuration is injected onto the device-orientation service', function(assert) {
   initialize(container, application);
   let deviceOrientation = application.__container__.lookup('service:device-orientation');
-  assert.deepEqual(deviceOrientation.get('orientationServiceDefaults'), application.__container__.lookup('config:device-orientation'), 'defaults are registered to service as "resizeServiceDefaults"');
+  assert.deepEqual(
+    deviceOrientation.get('orientationServiceDefaults'),
+    application.__container__.lookup('config:device-orientation'),
+    'defaults are registered to service as "resizeServiceDefaults"'
+  );
 });
-
