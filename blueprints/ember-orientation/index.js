05fa4c217487d2411a2a21dced08b1dacb2ab582
Mike North <michael.l.north@gmail.com>
2018-11-10 18:42:21 -0800

    fix: remove unused js file

--- blueprints/ember-orientation/index.js
+++ blueprints/ember-orientation/index.js
@@ -1,9 +1,9 @@
-/*jshint node:true*/
+/* eslint-env node */
 module.exports = {
   description: 'ember-orientation installation blueprint',
   normalizeEntityName: function() {},
 
-  afterInstall: function(options) {
+  afterInstall: function(/* options */) {
     return this.addAddonToProject('ember-getowner-polyfill', '^1.0.0');
   }
 };
